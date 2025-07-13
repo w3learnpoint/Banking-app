0e1d0aad1f3f5e374d08389a135485d7b6ef1bd8
w3learnpoint <sunil.fb94@gmail.com>
2025-07-13 13:28:06 +0530

    Update Deposit

--- client/src/pages/Deposit/Deposit.jsx
+++ client/src/pages/Deposit/Deposit.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { adminRoute } from '../../utils/router';
 const Deposit = () => {
-  const navigate = useNavigate();
   return (
     <div className="dashboard-wrapper px-4 py-4 theme-bg mt-3">
       <div className="row">
